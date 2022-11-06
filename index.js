04f0d9dc27d2e2d290f41e73c99fd41520c8e359
Ax-ritzy <112791751+Ax-ritzy@users.noreply.github.com>
2022-11-06 15:29:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ var moveinterval = 2; // 2 second movement interval
 var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
 var host = data["ip"];
 var username = data["name"]
-var nightskip = data["auto-night-skip"]
 var bot = mineflayer.createBot({
   host: host,
   port:data["port"],
@@ -40,55 +39,6 @@ bot.on('login',function(){
 	} 
 	console.log("Logged In")
 	bot.chat("hello");
-});
-  
-function task(i) { 
-	
-  setTimeout(function() { 
-    if(first == true){
-	bot.chat("Hi welcome To World of AXMC")
-	first = false;
-	}
-	else{
-		bot.chat("Good Luck on Your Journey")
-		first = true;
-	}
-  }, 3600000 * i); 
-} 
-
-
-bot.on('time', function(time) {
-
-		
-	if(nightskip == "true"){
-	if(bot.time.timeOfDay >= 13000){
-	bot.chat('/time set day')
-	}}
-    if (connected <1) {
-        return;
-    }
-    if (lasttime<0) {
-        lasttime = bot.time.age;
-    } else {
-        var randomadd = Math.random() * maxrandom * 20;
-        var interval = moveinterval*20 + randomadd;
-        if (bot.time.age - lasttime > interval) {
-            if (moving == 1) {
-                bot.setControlState(lastaction,false);
-                moving = 0;
-                lasttime = bot.time.age;
-            } else {
-                var yaw = Math.random()*pi - (0.5*pi);
-                var pitch = Math.random()*pi - (0.5*pi);
-                bot.look(yaw,pitch,false);
-                lastaction = actions[Math.floor(Math.random() * actions.length)];
-                bot.setControlState(lastaction,true);
-                moving = 1;
-                lasttime = bot.time.age;
-                bot.activateItem();
-            }
-        }
-    }
 });
 
 bot.on('spawn',function() {
